9b481f1b011cab517d094514a7e48891d05c0b7c
heatherzen <heather.zen@gmail.com>
2020-08-22 11:30:17 -0700

    finished main inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@ const logo = require('asciiart-logo');
 // const db = require('./db');
 //const { viewAllEmployees, addDepartment } = require('./db/indexdb');
 const inquirer = require('inquirer');
+const { addEmployee, updateEmployeeRole } = require('./db');
 require('console.table');
 
 init();
@@ -21,38 +22,69 @@ function init() {
                     {
                         name: "View All Departments",
                         value: "view_dept"
-                        //  "View All Roles", "View All Employees", "Add a Department", "Add a Roll", "Add an Employee", "Update Employee Role"]
-
+                    }, 
+                    {
+                        name: "View All Roles",
+                        value: "view_role"
+                    },
+                    {
+                        name: "View All Employees",
+                        value: "view_employees"
+                    },
+                    {
+                        name: "Add a Department",
+                        value: "add_dept"
+                    },
+                    {
+                        name: "Add a Roll",
+                        value: "add_role"
+                    },
+                    {
+                        name: "Add an Employee",
+                        value: "add_employee"
+                    },
+                    {
+                        name: "Update Employee Role",
+                        value: "update_employee"
                     }
                 ]
             }])
-    // .then(answer => {
+    .then(answer => {
 
-    //     switch(answer){
-    //         case 'View All Departments':
-    //             viewAllDepartments();
-    //             break;
-    //         case 'View All Roles':
-    //             viewAllRoles();
-    //             break;
-    //         case 'View All Employees':
-    //             viewAllEmployees();
-    //             break;
-    //         case 'Add a Department':
-    //             deptAdd();
-    //             break;
-    //     }
-    // })
+        switch(answer){
+            case 'view_dept':
+                viewAllDepartments();
+                break;
+            case 'view_role':
+                viewAllRoles();
+                break;
+            case 'view_employees':
+                viewAllEmployees();
+                break;
+            case 'add_dept':
+                deptAdd();
+                break;
+            case 'add_role':
+                addRole();
+                break;
+            case 'add_employee':
+                addEmployee();
+                break;
+            case 'update_employee':
+                updateEmployeeRole();
+                break;
+        }
+    })
 }
-// function deptAdd() {
-//     inquirer.prompt([
-//         {
+function deptAdd() {
+    inquirer.prompt([
+        {
 
-//             type: 'input',
-//             name: 'add_dept',
-//             message: 'What is the name of the Department?'
-//         }
-//     ]).then(deptAddAnswer => {
-//         addDepartment(deptAddAnswer)
-//     })
-// }
+            type: 'input',
+            name: 'add_dept',
+            message: 'What is the name of the Department?'
+        }
+    ]).then(deptAddAnswer => {
+        addDepartment(deptAddAnswer)
+    })
+}
